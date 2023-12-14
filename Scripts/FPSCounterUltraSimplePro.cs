67f8407b2212b6f2ced7d59ca5921b7ee59afb0a
akareactor <akareactor@mail.ru>
2023-12-14 13:43:16 +0300

    Добавил пространство имён namespace Kulibin.Space.FpsCounter и мета-файлы

--- Scripts/FPSCounterUltraSimplePro.cs
+++ Scripts/FPSCounterUltraSimplePro.cs
@@ -3,36 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class FPSCounterUltraSimplePro : MonoBehaviour {
+namespace Kulibin.Space.FpsCounter {
 
-	public static FPSCounterUltraSimplePro instance;
-	public bool persistent; // persist between scenes
-	public TextMeshProUGUI text;
-	public float rate = 0.5f;
-	float startTime;
-	float startFrame;
+	public class FPSCounterUltraSimplePro : MonoBehaviour {
 
-	void Awake () {
-		if (persistent) {
-			if (instance == null) {
-				DontDestroyOnLoad(gameObject);
-				instance = this;
-			}
-			else if (instance != this) {
-				DestroyImmediate(gameObject);
+		public static FPSCounterUltraSimplePro instance;
+		public bool persistent; // persist between scenes
+		public TextMeshProUGUI text;
+		public float rate = 0.5f;
+		float startTime;
+		float startFrame;
+
+		void Awake () {
+			if (persistent) {
+				if (instance == null) {
+					DontDestroyOnLoad(gameObject);
+					instance = this;
+				}
+				else if (instance != this) {
+					DestroyImmediate(gameObject);
+				}
 			}
 		}
-	}
-	private void Start () {
-		startTime = Time.realtimeSinceStartup;
-		startFrame = Time.frameCount;
-	}
-
-	void Update () {
-		if (Time.realtimeSinceStartup - startTime > rate) {
-			text.text = "FPS " + ((float)(Time.frameCount - startFrame) / (Time.realtimeSinceStartup - startTime)).ToString("#.0");
+		private void Start () {
 			startTime = Time.realtimeSinceStartup;
 			startFrame = Time.frameCount;
 		}
+
+		void Update () {
+			if (Time.realtimeSinceStartup - startTime > rate) {
+				text.text = "FPS " + ((float)(Time.frameCount - startFrame) / (Time.realtimeSinceStartup - startTime)).ToString("#.0");
+				startTime = Time.realtimeSinceStartup;
+				startFrame = Time.frameCount;
+			}
+		}
 	}
+
 }
